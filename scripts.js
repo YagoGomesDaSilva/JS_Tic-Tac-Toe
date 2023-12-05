03610a5cfdd9961f58853b7b2e4188b4cf577eb9
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2023-12-04 21:34:49 -0300

    Add modo de jogo

--- scripts.js
+++ scripts.js
@@ -1,15 +1,5 @@
 let isCircleTurn;
 
-const setQuerySelectors = () => {
-  const cellElements = document.querySelectorAll('[data-cell]');
-  const board = document.querySelector('[data-board]');
-  const winningMessage = document.querySelector('[data-winning-message]');
-  const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
-  const restartButton = document.querySelector('[data-restart-button]');
-
-  return { cellElements, board, winningMessage, winningMessageTextElement, restartButton };
-};
-
 const winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -21,7 +11,67 @@ const winningCombinations = [
   [2, 4, 6],
 ];
 
-const makeBoard = () => {
+const setQuerySelectors = () => {
+  const cellElements = document.querySelectorAll('[data-cell]');
+  const board = document.querySelector('[data-board]');
+  const winningMessage = document.querySelector('[data-winning-message]');
+  const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+  const restartButton = document.querySelector('[data-restart-button]');
+
+  return { cellElements, board, winningMessage, winningMessageTextElement, restartButton };
+};
+
+const playModeDialog = () => {  
+  const overlay = document.createElement('div');
+  const contener = document.createElement('div');
+  const text = document.createElement('p');
+  const buttonPVP = document.createElement('button');
+  const buttonPVE = document.createElement('button');
+
+  overlay.id = 'overlay';
+  contener.id = 'custom-prompt';
+  buttonPVP.id = 'PVP';
+  buttonPVE.id = 'PVE';
+
+  text.innerText = 'Escolha o modo de jogo:';
+  buttonPVP.textContent = 'PVP';
+  buttonPVE.textContent = 'PVE';
+
+  overlay.appendChild(text);
+  overlay.appendChild(contener);
+  overlay.appendChild(buttonPVP);
+  overlay.appendChild(buttonPVE);
+  document.body.appendChild(overlay);
+};
+
+
+const showPlayModeDialog = () => {
+  return new Promise((resolve) => {
+    let onMode;
+    const overlay = document.getElementById('overlay');
+    const buttonPVP = document.getElementById('PVP');
+    const buttonPVE = document.getElementById('PVE');
+    overlay.style.display = 'flex';
+
+    // Adiciona os listeners aos botões
+    buttonPVP.addEventListener('click', () => {
+      onMode = true;
+      overlay.style.display = 'none';
+      resolve(onMode); // Resolve a Promise quando o modo é escolhido
+    });
+
+    buttonPVE.addEventListener('click', () => {
+      onMode = false;
+      overlay.style.display = 'none';
+      resolve(onMode); // Resolve a Promise quando o modo é escolhido
+    });
+
+  });
+
+};
+
+
+const makeBoard = (onMode) => {
   // Cria o elemento div com a classe 'board' e o atributo 'data-board'
   const boardDiv = document.createElement('div');
   boardDiv.className = 'board';
@@ -156,11 +206,16 @@ const handleClick = (event) => {
   }
 };
 
-makeBoard();
-makeWinningMessage();
+playModeDialog();
 
-startGame();
+const runGame = async () => {
+  const onMode = await showPlayModeDialog();
+  makeBoard(onMode);
+  makeWinningMessage();
+  startGame();
+  const restartButton = document.querySelector('[data-restart-button]');
 
-const restartButton = document.querySelector('[data-restart-button]');
+  restartButton.addEventListener('click', () => {location.reload();} );
+};
 
-restartButton.addEventListener('click', startGame);
+runGame();
